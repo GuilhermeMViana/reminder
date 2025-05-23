31d6a3b9a859d56316f680fa12336d0cc06cef53
GuilhermeMViana <gmunizviana@gmail.com>
2025-05-23 17:32:45 -0300

    feat(SceneDelegate): adding SplashViewController

--- Reminder/Souces/Features/Splash/SplashView.swift
+++ Reminder/Souces/Features/Splash/SplashView.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+
+class SplashView: UIView {
+    init() {
+        super.init(frame: .zero)
+        
+        setupUI()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        self.backgroundColor = .red
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {}
+}
