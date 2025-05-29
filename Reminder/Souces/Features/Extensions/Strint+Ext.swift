23c112c555dda322b72a9d4ef35dfe7c70a638cf
GuilhermeMViana <gmunizviana@gmail.com>
2025-05-29 00:39:20 -0300

    feat(login): finished LogimBottomSheet and added Localizable to code

---
+++ Reminder/Souces/Features/Extensions/Strint+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  Strint+Ext.swift
+//  Reminder
+//
+//  Created by Guilherme Muniz Viana on 29/05/25.
+//
+
+import Foundation
---
+++ Reminder/Souces/Features/LoginBottomSheet/LoginBottomSheetViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginBottomSheetViewController.swift
+//  Reminder
+//
+//  Created by Guilherme Muniz Viana on 27/05/25.
+//
+
+import Foundation
